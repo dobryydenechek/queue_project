2cd06e55fbab8d50183e029feb70e861b2a82da9
Karen Karamyan <meeporen@gmail.com>
2025-04-27 20:22:01 +0300

    ScheduleCommit

--- src/main/java/com/example/queue_project/repository/GroupRepository.java
+++ src/main/java/com/example/queue_project/repository/GroupRepository.java
@@ -1,9 +1,13 @@
 package com.example.queue_project.repository;
 
-import com.example.queue_project.repository.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     boolean existsByGroupId(Long groupId);
+
+    @Query("SELECT g.groupId FROM Group g")
+    List<Long> findAllGroupIds();
 }
